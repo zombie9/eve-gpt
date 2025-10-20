3345c273ea8b90e81bc9768c96a847aaa4522663
Darryl Banks <darryl.banks@sky.uk>
2025-10-20 16:23:27 +0100

    chore: enable @ConfigurationPropertiesScan for custom property metadata

--- src/main/java/com/evegpt/EveGptApplication.java
+++ src/main/java/com/evegpt/EveGptApplication.java
@@ -2,8 +2,10 @@ package com.evegpt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class EveGptApplication {
 
     public static void main(String[] args) {
